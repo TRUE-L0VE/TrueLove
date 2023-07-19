044b11de124fbd381fd82b25242b10ce5ddafeef
지율 <516938@naver.com>
2023-07-19 14:15:47 +0900

    STT 구현

---
+++ Web-pair/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function(){
+    console.log('listening on 8080')
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/home.html')
+})
+
+app.get('/home', function(req, res){
+    res.sendFile(__dirname + '/home.html')
+})
+
+app.get('/test', function(req, res){
+    res.sendFile(__dirname + '/test.html');
+})
+
+app.get('/chat', function(req, res){
+    res.sendFile(__dirname + '/chat.html');
+})
+
+app.get('/info', function(req, res){
+    res.sendFile(__dirname + '/info.html');
+})
+
+app.get('/chat_Man_T', function(req, res){
+    res.sendFile(__dirname + '/chat_Man_T.html');
+})
+
+app.get('/chat_Man_F', function(req, res){
+    res.sendFile(__dirname + '/chat_Man_F.html');
+})
+
+app.get('/chat_Woman_T', function(req, res){
+    res.sendFile(__dirname + '/chat_Woman_T.html');
+})
+
+app.get('/chat_Woman_F', function(req, res){
+    res.sendFile(__dirname + '/chat_Woman_F.html');
+})
