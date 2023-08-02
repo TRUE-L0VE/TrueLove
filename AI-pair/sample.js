730e04858622287fb8a0d7b57662bf1db881831e
catuscio <catuscio@hotmail.com>
2023-08-02 15:18:34 +0900

    중간점검

--- AI-pair/sample.js
+++
@@ -1,9 +0,0 @@
-OPENAI_API_KEY = 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;
-
-import { Configuration, OpenAIApi } from "openai";
-const configuration = new Configuration({
-    organization: "org-dqjTq1XwGm2xi9EzTaPyEWmM",
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-const response = await openai.listModels();
