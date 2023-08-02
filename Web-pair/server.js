b4178d0d5fe0edffd196389079e2cf7fd4e076f7
지율 <516938@naver.com>
2023-08-02 15:23:34 +0900

    채팅 오류 버전

--- Web-pair/server.js
+++ Web-pair/server.js
@@ -40,3 +40,7 @@ app.get('/chat_Woman_T', function(req, res){
 app.get('/chat_Woman_F', function(req, res){
     res.sendFile(__dirname + '/chat_Woman_F.html');
 })
+
+app.get('/codingapple', function(req, res){
+    res.sendFile(__dirname + '/codingapple.html');
+})
